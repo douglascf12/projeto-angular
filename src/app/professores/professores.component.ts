55020abcd8ab464e37abf15bd7aeee0ef3be8cbc
Douglas Cardoso <douglascf12@live.com>
2021-03-12 18:48:56 -0300

    ngIf add no component professores

--- src/app/professores/professores.component.ts
+++ src/app/professores/professores.component.ts
@@ -9,6 +9,8 @@ export class ProfessoresComponent implements OnInit {
 
   titulo = 'Professores';
 
+  public professorSelecionado!: string;
+
   public professores = [
     { id: 1, nome: 'Lauro', disciplina: 'Matemática' },
     { id: 2, nome: 'Roberto', disciplina: 'Física' },
@@ -16,6 +18,14 @@ export class ProfessoresComponent implements OnInit {
     { id: 4, nome: 'Rodrigo', disciplina: 'Inglês' },
     { id: 5, nome: 'Alexandre', disciplina: 'Programação' },
   ];
+
+  professorSelect(professor: any) {
+    this.professorSelecionado = professor.nome;
+  }
+
+  voltar() {
+    this.professorSelecionado = '';
+  }
 
   constructor() { }
 
