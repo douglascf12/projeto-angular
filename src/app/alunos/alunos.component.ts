b5a2d108d2f9f0bef862e9247a882ff748287647
Douglas Cardoso <douglascf12@live.com>
2021-03-12 17:45:57 -0300

    table bootstrap add em aluno e professores

--- src/app/alunos/alunos.component.ts
+++ src/app/alunos/alunos.component.ts
@@ -10,13 +10,13 @@ export class AlunosComponent implements OnInit {
   public titulo = 'Alunos';
 
   public alunos = [
-    { nome: 'Marta' },
-    { nome: 'Paula' },
-    { nome: 'Laura' },
-    { nome: 'Luiza' },
-    { nome: 'Lucas' },
-    { nome: 'Pedro' },
-    { nome: 'Paulo' },
+    { id: 1, nome: 'Marta', sobrenome: 'Kent', telefone: 11110000 },
+    { id: 2, nome: 'Paula', sobrenome: 'Isabela', telefone: 22220000 },
+    { id: 3, nome: 'Laura', sobrenome: 'Antonia', telefone: 33330000 },
+    { id: 4, nome: 'Luiza', sobrenome: 'Maria', telefone: 44440000 },
+    { id: 5, nome: 'Lucas', sobrenome: 'Machado', telefone: 55550000 },
+    { id: 6, nome: 'Pedro', sobrenome: 'Alvares', telefone: 66660000 },
+    { id: 7, nome: 'Paulo', sobrenome: 'Jose', telefone: 77770000 },
   ];
 
   constructor() { }
