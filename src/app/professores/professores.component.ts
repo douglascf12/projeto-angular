ca0abb0ea0205ff1f0f0bc7b0c611c4004e7e6bc
Douglas Cardoso <douglascf12@live.com>
2021-03-13 13:55:12 -0300

    add reactive form em professor

--- src/app/professores/professores.component.ts
+++ src/app/professores/professores.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Professor } from '../models/Professor';
 
 @Component({
@@ -8,7 +9,8 @@ import { Professor } from '../models/Professor';
 })
 export class ProfessoresComponent implements OnInit {
 
-  titulo = 'Professores';
+  public professorForm!: FormGroup;
+  public titulo = 'Professores';
   public professorSelecionado!: Professor;
   public professorNull!: Professor;
 
@@ -20,17 +22,33 @@ export class ProfessoresComponent implements OnInit {
     { id: 5, nome: 'Alexandre', disciplina: 'Programação' },
   ];
 
+  constructor(private fb: FormBuilder) {
+    this.criarForm();
+  }
+
+  ngOnInit() {
+  }
+
+  criarForm() {
+    this.professorForm = this.fb.group({
+      nome: ['', Validators.required],
+      disciplina: ['', Validators.required]
+    });
+  }
+
+  professorSubmit() {
+    console.log(this.professorForm.value);
+  }
+
   professorSelect(professor: Professor) {
     this.professorSelecionado = professor;
+    this.professorForm.patchValue(professor);
   }
 
   voltar() {
     this.professorSelecionado = this.professorNull;
   }
 
-  constructor() { }
 
-  ngOnInit() {
-  }
 
 }
